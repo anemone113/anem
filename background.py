1b2b7c8acdb401cff1481f5cdb1194b608718256
sylar112 <35702373+sylar112@users.noreply.github.com>
2024-10-23 06:28:18 +0300

    background.py

--- background.py
+++ background.py
@@ -1,19 +1,20 @@
 from flask import Flask
 from flask import request
+from flask_cors import CORS  # Импортируем CORS
 from threading import Thread
 import time
 import requests
 
-
 app = Flask('')
+CORS(app, resources={r"/*": {"origins": "https://anemone.onrender.com"}})  # Настройка CORS
 
 @app.route('/')
 def home():
-  return "I'm alive"
+    return "I'm alive"
 
 def run():
-  app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+    t = Thread(target=run)
+    t.start()
