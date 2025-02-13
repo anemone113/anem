128b3f7c1454dfadaa756f7bcbd24e7d135fd1ea
sylar112 <35702373+sylar112@users.noreply.github.com>
2025-02-13 12:25:31 +0300

    background.py

--- background.py
+++ background.py
@@ -6,7 +6,22 @@ import requests
 app = Flask('')
 @app.route('/')
 def home():
-  return "I'm alive"
+    return "I'm alive"
+
+@app.route('/map')
+def show_map():
+    return '''
+    <html>
+    <head>
+        <title>Карта</title>
+    </head>
+    <body>
+        <iframe width="100%" height="500px" frameborder="0" allowfullscreen allow="geolocation"
+        src="https://umap.openstreetmap.fr/ru/map/?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true"></iframe>
+        <p><a href="https://umap.openstreetmap.fr/ru/map/?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true">Смотреть в полноэкранном режиме</a></p>
+    </body>
+    </html>
+    '''
 def run():
   app.run(host='0.0.0.0', port=80)
 def keep_alive():
