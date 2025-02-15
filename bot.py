641b563d951278102d07e5e81f23e8fc51a9ba20
sylar112 <35702373+sylar112@users.noreply.github.com>
2025-02-15 06:36:55 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -4118,10 +4118,11 @@ async def show_map(update: Update, context: CallbackContext):
     
     umap_url = await view_map()
     webapp_url = "https://umap.openstreetmap.fr/ru/map/anemone_1177482?scaleControl=true&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=false&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true&measureControl=true&datalayers=ad0d1cbb-4dd3-4476-8111-c8a40f97126b%2Ca5a444be-fdb5-44aa-81a7-2a0c4b889705&locateControl=true&starControl=false#9/55.6147/37.3123"  # URL твоей карты
-
+    youtube_url = "https://m.youtube.com"
     keyboard = [
         [InlineKeyboardButton("Посмотреть на карте", url=umap_url)],
-        [InlineKeyboardButton("Открыть в WebApp", web_app=WebAppInfo(url=webapp_url))]
+        [InlineKeyboardButton("Открыть в WebApp", web_app=WebAppInfo(url=webapp_url))],
+        [InlineKeyboardButton("YouTube", web_app=WebAppInfo(url=youtube_url))],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     
